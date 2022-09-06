5e6f355866f7ccfa6cc002df34359769c8b57ea3
Dariusz Karp <heniulu10@gmail.com>
2022-09-06 17:43:13 +0200

    feat: add main address for mapping movies at the top of MovieController class
    good practice to reduce the code of @GetMapping annotations

--- src/main/java/com/drekyyy/movielibrary/MovieController.java
+++ src/main/java/com/drekyyy/movielibrary/MovieController.java
@@ -3,22 +3,24 @@ package com.drekyyy.movielibrary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/movies")
 public class MovieController {
 
     @Autowired
     MovieRepository movieRepository;
 
-    @GetMapping("/movies")
+    @GetMapping("/")
     public List<Movie> getAll(){
         return movieRepository.getAll();
     }
 
-    @GetMapping("/movies/{id}")
+    @GetMapping("/{id}")
     public Movie getById(@PathVariable("id") int id){
         return movieRepository.getById(id);
     }
